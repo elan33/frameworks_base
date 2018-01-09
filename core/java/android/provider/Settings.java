71fc9288699804a71a3cfd284aa39ba5bc3a9f43
xyyx <xyyx@mail.ru>
2018-01-09 19:36:56 +0100

    Unlock keystore with fingerprint after reboot (1/2)
    Change-Id: I839e6764760cee1a309c47e73ae58a337b033d7a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4690,6 +4690,11 @@ public final class Settings {
          */
         public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
 
+	/** Unlock keystore with fingerprint after reboot
+         * @hide
+         */
+        public static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
