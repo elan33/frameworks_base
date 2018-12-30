3be90ef827094a04b11306b8caa62b8aa6ee1ce9
sdv22 <sdv22@live.ru>
2018-12-30 20:43:13 +0300

    Smartlock auth by LID and GESTURE
    Change-Id: I0396b94dd1e4479f0a5d3e2d060aa556a1aaece6

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -1138,6 +1138,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         String wakeupReason = SystemProperties.get("power.wake_reason","unknown");
 
         if( wakeupReason.equals("android.policy:POWER") ||
+            wakeupReason.equals("android.policy:LID") ||
+            wakeupReason.equals("android.policy:GESTURE") ||
             wakeupReason.equals("com.android.systemui:NODOZE") ||
             wakeupReason.equals("wakelock:qfp-service") ) {
 
